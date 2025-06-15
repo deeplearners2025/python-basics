cb7b803b5b0ccce4cbc3dff319918eaeede33b09
GGG <deeplearners2025@gmail.com>
2025-06-15 17:11:50 +0530

    Print Even numbers 1 to N

--- Gaurav/evenNumbers1toN.py
+++ Gaurav/evenNumbers1toN.py
@@ -1,6 +1,11 @@
 n = int(input("Enter the number to print the Range to:"))
 
 print(f"Printing Even numbers from 1 to {n}")
-for i in range(1,n):
+for i in range(1,n+1):
+    # Method 1 using if
     if i%2 == 0:
+        print(i)
+    # Method 2 using lambda function
+    evenNumber=(lambda i:i%2==0)
+    if evenNumber(i):
         print(i)
